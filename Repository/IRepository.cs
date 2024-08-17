bc8c96f150b7624760bbc609e5902341c2f95a90
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-17 18:02:23 +0100

    create repositories

---
+++ Repository/IRepository.cs
@@ -0,0 +1,12 @@
+using System.Linq.Expressions;
+
+public interface IRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+
+    Task<T> GetByValueAsync(Expression<Func<T, bool>> predicate);
+}
