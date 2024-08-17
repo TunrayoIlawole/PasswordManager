b42055c9682b8b22e283feb799b7650c11c239c4
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-17 18:03:01 +0100

    create login dto

---
+++ Models/DTOs/LoginDto.cs
@@ -0,0 +1,10 @@
+
+namespace PasswordManager.DTOs {
+
+    public class LoginDto {
+
+        public required string Email  { get; set; }
+        public required string Password  { get; set; }
+        
+    }
+}
