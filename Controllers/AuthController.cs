0f12ccbfd03c136802d9f36a12b907c12ef35796
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-18 18:25:33 +0100

    update login method

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,16 +4,27 @@ using System.Collections.Generic;
 using AutoMapper;
 using PasswordManager.DTOs;
 using BCrypt.Net;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace PasswordManager.Controllers
 {
+    [ApiController]
     public class AuthController : ControllerBase {
         private readonly IRepository<User> _userRepository;
         private readonly IMapper _mapper;
+        private readonly string _secretKey;
+        private readonly string _issuer;
+        private readonly string _audience;
 
-        public AuthController(IRepository<User> userRepository, IMapper mapper) {
+        public AuthController(IRepository<User> userRepository, IMapper mapper, IConfiguration configuration) {
             _userRepository = userRepository;
             _mapper = mapper;
+            _secretKey = configuration["Jwt:SecretKey"];
+            _issuer = configuration["Jwt:Issuer"];
+            _audience = configuration["Jwt:Audience"];
         }
 
         [HttpPost("login")]
@@ -22,17 +33,33 @@ namespace PasswordManager.Controllers
 
             if (user != null) {
                 if (BCrypt.Net.BCrypt.Verify(data.Password, user.Password)) {
-                    return Ok(new { Token = });
+                    return Ok(new { Token = GenerateToken(data.Email)});
                 }
-
-// To be updated?
-                return BadRequest();
+                return Unauthorized();                    
             }
             return NotFound();
 
         }
 
-        
+        private string GenerateToken(string email) {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Email, email)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                Issuer = _issuer,
+                Audience = _audience,
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+        }
 
     }
 }
