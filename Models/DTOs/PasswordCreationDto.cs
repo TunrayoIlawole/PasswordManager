646f68007c9839b28909df4091bce4dcf8f45f17
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-19 18:00:55 +0100

    create password creation dto

---
+++ Models/DTOs/PasswordCreationDto.cs
@@ -0,0 +1,5 @@
+public class PasswordCreationDto {
+    public required string WebsiteUrl { get; set; }
+    public required string EmailOrUsername { get; set; }
+    public required string WebsitePassword { get; set; }
+}
