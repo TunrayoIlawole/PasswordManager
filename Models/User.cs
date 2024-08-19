fa487f7e1fd301cfb0e675f9e682269db3439c8d
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-19 13:38:56 +0100

    update user model

--- Models/User.cs
+++ Models/User.cs
@@ -7,10 +7,10 @@ namespace PasswordManager.Models
         public int Id { get; set; }
 
         [Required]
-        public string Username { get; set; }
+        public string Email { get; set; }
 
         [Required]
-        public string Email { get; set; }
+        public string Username { get; set; }
 
         [Required]
         public string Password { get; set; }
