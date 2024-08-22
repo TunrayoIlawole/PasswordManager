0c5504a6bfbb7052b37735c83a356f47f8fb4eb7
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-22 18:25:34 +0100

    update response messages

--- Responses/ResponseMessages.cs
+++ Responses/ResponseMessages.cs
@@ -7,9 +7,13 @@ namespace PasswordManager.Responses {
         public static string InvalidPassword => "Password not found";
 
         public static string InvalidPasswords => "Passwords not found";
+
+        public static string Unauthorized => "You are not authorized to access this resource";
          public static string InvalidClaim(string claimName) => $"The {claimName} is missing";
 
         public static string DuplicateUser(string email) => $"User with email {email} already exists";
+
+        public static string SucessfullAction(string entity, string action) => $"{entity} {action} successfully";
         
     }
 }
